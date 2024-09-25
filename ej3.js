70b6454387f7af8c62d80a6372bcb668fae89add
FranMorales7 <162159997+FranMorales7@users.noreply.github.com>
2024-09-25 13:37:44 +0200

    Update ej3.js

--- ej3.js
+++ ej3.js
@@ -1,7 +1,7 @@
 let numeros = [5, 10, 15, 20, 25];
-let aux = 0;
-numeros.forEach(function(numero) {
+let aux = numeros.forEach(function(numero) {
     aux += numero;
 });
 
 console.log("Suma total -->",aux);
+
