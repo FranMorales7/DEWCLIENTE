c28666f589760a696df675ae10722a99f25ee147
FranMorales7 <162159997+FranMorales7@users.noreply.github.com>
2024-09-25 13:38:09 +0200

    Update ej4.js

--- ej4.js
+++ ej4.js
@@ -1,12 +1,8 @@
 let numeros = [1,2,3,4,5,6,7,8,9,10];
 
-function esPar(numeros){
-    numeros.forEach((elemento) => {
+let esPar = numeros.forEach((elemento) => {
         if (elemento % 2 == 0){
             return console.log(elemento);
         } 
     });
-}
-esPar(numeros);
-let aux = numeros.filter(esPar);
-console.log(aux);
+console.log(esPar);
